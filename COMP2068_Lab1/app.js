0250656e7338cde0d00e5755da0b8b8b636dfd91
Alap Dhruva <alapdhruva1996@gmail.com>
2020-05-28 15:42:13 -0400

    my third commit

--- COMP2068_Lab1/app.js
+++ COMP2068_Lab1/app.js
@@ -1,4 +1,7 @@
 'use strict';
 
 console.log('Hello Alap');
-console.log('I am enjoying JavaScript!');
+console.log('I am enjoying JavaScript!'); 
+console.log('I will try to get good score!'); 
+
+
