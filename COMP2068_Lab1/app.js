7d2bb21ac4e47456fb36903c6b4660214b6bbe3f
Alap Dhruva <alapdhruva1996@gmail.com>
2020-05-28 21:18:18 -0400

    last ommit

--- COMP2068_Lab1/app.js
+++ COMP2068_Lab1/app.js
@@ -4,6 +4,7 @@ console.log('Hello Alap');
 console.log('I am enjoying JavaScript!'); 
 console.log('I will try to get good score!'); 
 
-console.log('four commit..just tring advanced'); 
+console.log('four commit..just tring advanced');
+console.log('id:200420045');
 
 
